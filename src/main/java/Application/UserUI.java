e7c2a67652e939bc2e1104f1217304a14c683ed6
Lucid <100102668+dearmysolitude@users.noreply.github.com>
2024-01-03 19:08:23 +0900

    UserUI upload

---
+++ src/main/java/Application/UserUI.java
@@ -0,0 +1,59 @@
+package Application;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class UserUI {
+    private BufferedReader br;
+    public UserUI() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+    
+    public int menu() {
+        System.out.println("1. 회원 등록");
+        System.out.println("2. 회원 목록 보기");
+        System.out.println("3. 종료");
+        int menuId = -1;
+        
+        try {
+            String line = br.readLine();
+            menuId = Integer.parseInt(line);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        return menuId;
+    }
+    
+    public User regMenu() {
+        try {
+            System.out.println("email을 입력하세요");
+            String email = br.readLine();
+            System.out.println("이름을 입력하세요.");
+            String name = br.readLine();
+            System.out.println("생년을 입력하세요.");
+            String strBirthYear = br.readLine();
+            int birthYear = Integer.parseInt(strBirthYear);
+
+            return new User(email, name, birthYear);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    
+    public void printUserList(List<User> users) {
+        System.out.println("email            이름            생년");
+        System.out.println("====================================");
+        for(int i = 0; i < users.size(); i ++) {
+            User user = users.get(i);
+            System.out.print(user.getEmail());
+            System.out.print("           ");
+            System.out.print(user.getName());
+            System.out.print("           ");
+            System.out.print(user.getBirthYear());
+
+        }
+    }
+}
