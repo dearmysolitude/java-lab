1a020e649a6170f95fd63f1e9193b31aa666823b
Lucid <100102668+dearmysolitude@users.noreply.github.com>
2024-01-04 09:54:07 +0900

    [Feat] User info edit feature added

--- src/main/java/membership/management/UserMain.java
+++ src/main/java/membership/management/UserMain.java
@@ -7,6 +7,10 @@ public class UserMain {
         UserUI userUI = new UserUI();
         UserDao userDao = new UserDao("./tmp/users.dat");
         List<User> users = userDao.getUsers();
+        executeUI(userUI, userDao, users);
+    }
+
+    private static void executeUI(UserUI userUI, UserDao userDao, List<User> users) {
         while(true) {
             int menuId = userUI.menu(); 
             if(menuId == 5) {
@@ -19,8 +23,12 @@ public class UserMain {
                 System.out.println("등록되었습니다.");
             } else if(menuId == 2) {
                 userUI.printUserList(users);
+            } else if(menuId == 3) {
+                System.out.println("수정할 유저의 일련 번호를 입력하세요.");
+                userUI.printUserList(users);
+                int memberId = -1;
+                userUI.editUser(users.get(memberId));
             }
         }
-
     }
 }
--- src/main/java/membership/management/UserUI.java
+++ src/main/java/membership/management/UserUI.java
@@ -13,6 +13,7 @@ public class UserUI {
     public int menu() {
         System.out.println("1. 회원 등록");
         System.out.println("2. 회원 목록 보기");
+        System.out.println("3. 회원 수정하기");
         System.out.println("5. 종료");
         int menuId = -1;
         
@@ -44,16 +45,37 @@ public class UserUI {
     }
     
     public void printUserList(List<User> users) {
-        System.out.println("email                  이름                 생년");
-        System.out.println("====================================================");
+        System.out.println("MemberID                   email                  이름                 생년");
+        System.out.println("============================================================================");
         for(int i = 0; i < users.size(); i ++) {
             User user = users.get(i);
+            System.out.println(i);
+            System.out.print("           ");
             System.out.print(user.getEmail());
             System.out.print("           ");
             System.out.print(user.getName());
             System.out.print("           ");
             System.out.println(user.getBirthYear());
             System.out.println();
+        }
+    }
+    
+    public void editUser(User user) {
+        
+        try {
+            System.out.println("email을 입력하세요");
+            String email = br.readLine();
+            System.out.println("이름을 입력하세요.");
+            String name = br.readLine();
+            System.out.println("생년을 입력하세요.");
+            String strBirthYear = br.readLine();
+            int birthYear = Integer.parseInt(strBirthYear);
+            
+            user.setEmail(email);
+            user.setName(name);
+            user.setBirthYear(birthYear);
+        } catch(Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
